14550d71fcc98d309b6c0ab40194825dee1e9c17
Zachary Taylor <Zacharytaylor0508@gmail.com>
2024-02-20 18:35:42 +0000

    mm

--- MH.py
+++ MH.py
@@ -1,19 +1,15 @@
-def main(): 
-   FF()
-def FF():
-   e= 0
-   while(e!=1):
-      H = int(input("Enter a hour 0 - 23\n"))
-      M = 0
-      if(H>23 or H < 0):
-         print("Error: Value out of range")
-      elif(H<24 and H > 0):
-         M = int(input("Enter the minutes 0 - 59\n"))
-      if(M > 59  or M < 0):
-         print("Error: Value out of range")
-      elif(M < 60 and M >= 0):
-         if(H < 24 and H >= 0 and M <60 and M >= 0):
-            print(f"You entered {H} Hours and {M} Minutes")
+def main():
+   hour = int(input("Enter a hour 0 - 23\n"))
+   min = int(input("Enter the minutes 0 - 59\n"))
+   hour,min = FF(hour,min)
+   print(f"You entered {hour} Hours and {min} Minutes")
+
+def FF(M,H):  
+   while(H>23 or H < 0):
+      print("Error: Value out of range")
+   while(M > 59  or M < 0):
+      print("Error: Value out of range")
+   return (M,H)
 main()        
 
  
