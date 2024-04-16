66fda99c556f40916a20bd1f7e0568dee6104ecf
DEKE21 <Zacharytaylor0508@gmail.com>
2024-04-15 20:30:44 -0500

    HW10

--- python/hw10.py
+++ python/hw10.py
@@ -1,1 +1,7 @@
- 
+import calendar
+date = input('Type a date in MM/DD/YYYY format: ')
+DL = date.split("/")
+month = calendar.month_name[int(DL[0])]
+day = DL[1]
+year = DL[2]
+print(f'{month} {day}, {year}')
